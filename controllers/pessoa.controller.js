7682cd652ed3e681d0418d1dcf1eea5b640b359f
Jairo Santos <107869809+JairoTSantos@users.noreply.github.com>
2024-08-07 12:59:57 -0300

    first commit

--- controllers/pessoa.controller.js
+++ controllers/pessoa.controller.js
@@ -8,7 +8,7 @@ class PessoaController {
         try {
             const pessoa = req.body;
 
-            if (!pessoa.pessoa_nome || !pessoa.pessoa_email || !pessoa.pessoa_municipio || !pessoa.pessoa_estado || !pessoa.pessoa_estado || !pessoa.pessoa_tipo || !pessoa.pessoa_orgao  || !pessoa.pessoa_criada_por) {
+            if (!pessoa.pessoa_nome || !pessoa.pessoa_email || !pessoa.pessoa_municipio || !pessoa.pessoa_estado || !pessoa.pessoa_estado || !pessoa.pessoa_tipo || !pessoa.pessoa_orgao || !pessoa.pessoa_criada_por) {
                 return res.status(400).json({ status: 400, message: 'Todos os campos obrigatórios devem ser preenchidos' });
             }
 
